1d04ebb51e0efab990e0b0df8c6af9a976059316
smadamas <55815178+smadamas@users.noreply.github.com>
2022-04-29 21:26:32 -0400

    updates for javasxcript

---
+++ js/main.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+    calcScrollLength();
+  };
+
+function calcScrollLength() {
+    var sum = 0;
+
+    console.log("Calcing scroll length...");
+
+    require(['fs','image-size'], function runCalculations(){
+        const dir = "../img/carousel"
+        const files = fs.readdirSync(dir)
+
+        for (const file of files) {
+            sizeOf(file, function (err, dimensions) {
+                sum += (dimensions.width/(dimensions.height/250))
+            });
+        }
+        console.log(sum);
+    });
+
+    
+    console.log("Done!");
+}
