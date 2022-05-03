c36dbc272ebf8bd6a4e0fdc64bd6d9fc5e51fea8
smadamas <55815178+smadamas@users.noreply.github.com>
2022-05-02 22:46:25 -0400

    finish site :)

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const quotes = ["\"I want to help keep the world beautiful\"",
         "\"I want to make a good life possible for everyone\"", 
         "\"I want to use technology and science as\n a solution for sustainability\"", 
         "\"I want to help build projects that\n improve the world\"",
-        "\"I want to apply nature to\n complex systems and problems\"",
+        "\"I want to apply natural sciences to\n complex systems and problems\"",
         "\"I want to contribute to a bright future\"",
         "\"I want to learn analytical and creative skills\"",
         "\"I want to disrupt the status quo\"",
@@ -20,10 +20,20 @@ const quotes = ["\"I want to help keep the world beautiful\"",
 
 const spacing = ["15%", "31%", "58%", "75%", "18%", "45%", "68%", "85%"];
 
+var prevQuoteNum = [99, 99];
+var prevPosition = 99;
+
 function randomizeQuotes() {
 
     var whichQuote = (Math.floor(Math.random() * 11))
     var whichPosition = (Math.floor(Math.random() * 8))
+    while (whichQuote == prevQuoteNum[0] || whichQuote == prevQuoteNum[1]){ //No repeats from previous 2
+        whichQuote = (Math.floor(Math.random() * 11))
+    }
+    while (whichPosition == prevPosition){ //No repeats from previous position
+        whichPosition = (Math.floor(Math.random() * 8))
+    }
+    prevPosition = whichPosition;
 
     document.getElementById("quote").innerHTML = quotes[whichQuote];
 
